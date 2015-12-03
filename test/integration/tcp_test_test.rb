7aeb9ef97e8895ea41ae371b70d289a6b403fe32
Thiago Scalone <thiago@scalone.com.br>
2015-12-03 11:01:12 -0200

    Fix TcpTestTest assertion.

--- test/integration/tcp_test_test.rb
+++ test/integration/tcp_test_test.rb
@@ -1,6 +1,6 @@
-class MainTest < DaFunk::Test.case
-  def test_true
-    assert Main.call
+class TcpTestTest < DaFunk::Test.case
+  def test_perform_tcp
+    assert_equal "CLOUDWALK TEST 1234567890\n", TcpTest.perform
   end
 end
 
