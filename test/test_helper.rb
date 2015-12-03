3e3c0f96b6c29fd65f3e0c8945ea0ed345528437
Thiago Scalone <thiago@scalone.com.br>
2015-12-03 11:00:27 -0200

    Configure test_helper.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,6 +5,10 @@ $LOAD_PATH.unshift "./#{APP_NAME}"
 require 'da_funk'
 
 DaFunk::Test.configure do |t|
-  t.root_path = ROOT_PATH
+  t.root_path      = ROOT_PATH
+  t.name           = APP_NAME
+  t.logical_number = "003"
+  t.serial         = "0000000003"
 end
 
+Device::Setting.to_staging!
